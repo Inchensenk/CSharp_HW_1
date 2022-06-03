c0e96c403862befdbe2275dd76e4b55a7415df3c
sofoyan gevork <aab81@bk.ru>
2022-06-03 10:44:34 +0300

    CSharp_HW_4_Students classes Subject and Student

---
+++ CSharp_HW_4_Students/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_HW_4_Students
+{
+    internal class Student
+    {
+        /// <summary>
+        /// Имя студента
+        /// </summary>
+        private string name;
+
+        /// <summary>
+        ///  Возвращает имя студента
+        /// </summary>
+        private string Name { get;}
+
+        /// <summary>
+        /// Конструктор с параметром
+        /// </summary>
+        /// <param name="name">Имя студента</param>
+        public Student(string name)
+        {
+            this.name = name;
+        }
+    }
+}
---
+++ CSharp_HW_4_Students/Subject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_HW_4_Students
+{
+    internal class Subject
+    {
+        /// <summary>
+        /// Название предмета
+        /// </summary>
+        private string name;
+
+        /// <summary>
+        /// Возвращает название предмета
+        /// </summary>
+        private string Name { get;}
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="name">Название предмета</param>
+        Subject(string name)
+        {
+            this.name = name;
+        }
+    }
+}
